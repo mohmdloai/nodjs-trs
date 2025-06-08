3b6f71064cd7d9335cd630bd7fe44f460a32b5aa
mohmdloai <mhmd.00.eng@gmail.com>
2025-06-08 18:27:19 +0300

    Made differenct routes

--- server.js
+++ server.js
@@ -3,13 +3,33 @@ const PORT = process.env.PORT;
 // You will send from the server to the client (ur browser !)
 const server = http.createServer((req, res) => {
 
-  res.writeHead(404, { "Content-Type": "application/json" });
-  var data = {
-    hello_key:
-      "Saying Hi from the node server, in watch mode  using end directly!",
-  };
+  if(req.url === '/'){
+    res.writeHead(200, { "Content-Type": "text/html" });
+    var data ="<h1>Saying Hi from the node server, in watch mode  using end directly!</h1>";
+    res.end(data);
 
-  res.end(JSON.stringify(data));
+
+  }else if(req.url === '/about')
+    {
+      res.writeHead(200,{'Content-Type':'text/html'});
+      data = "<h1>About Page</h1>";
+      res.end(data);
+
+    }else if(req.url === '/contact')
+      {
+        res.writeHead(200,{'Content-Type':'text/html'});
+        data = "<h1>This is conact Page</h1>";
+        res.end(data);
+
+      } else {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      data = "<h1>Page Not Found</h1>";
+      res.end(data);
+
+
+
+    }
+
 });
 
 server.listen(PORT, () => {
