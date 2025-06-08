87b5ad10142a2eb79040c6c9d3b59f1bd494fa16
mohmdloai <mhmd.00.eng@gmail.com>
2025-06-08 16:03:33 +0300

    Added .env while running,and used PORT from it

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-import http from 'http';
-const PORT=5000;
+import http from "http";
+const PORT = process.env.PORT;
 // You will send from the server to the client (ur browser !)
-const server = http.createServer((req, res)=>{
-    // res.writeHead(200, {'Content-Type': 'plain/text'});
-    // res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.setHeader('Content-Type', 'text/html')
-    res.writeHead(404, {'Content-Type': 'application/json'});
-    var data = {"hello_key":"Saying Hi from the node server, in watch mode  using end directly!"};
-    // res.write({"hello_key":"Saying Hi from the node server, in watch mode  !"});
-    // res.write();
-    // res.statusCode= 404; 
-    res.end(JSON.stringify(data));
-}
-);
+const server = http.createServer((req, res) => {
 
-server.listen(PORT, ()=>{
-    console.log(`server runinig on port ${PORT}`);
-}
-);
+  res.writeHead(404, { "Content-Type": "application/json" });
+  var data = {
+    hello_key:
+      "Saying Hi from the node server, in watch mode  using end directly!",
+  };
+
+  res.end(JSON.stringify(data));
+});
+
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
