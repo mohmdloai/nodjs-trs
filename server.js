8c5b65067d1fce6086cccb4e32a83fc1e4050897
mohmdloai <mhmd.00.eng@gmail.com>
2025-06-08 15:45:47 +0300

    Used writeHead setHeader , write and end directly too

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import http from 'http';
 const PORT=5000;
 // You will send from the server to the client (ur browser !)
 const server = http.createServer((req, res)=>{
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var data = {"hello_key":"Saying Hi from the node server, in watch mode  !"};
+    // res.writeHead(200, {'Content-Type': 'plain/text'});
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.setHeader('Content-Type', 'text/html')
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    var data = {"hello_key":"Saying Hi from the node server, in watch mode  using end directly!"};
     // res.write({"hello_key":"Saying Hi from the node server, in watch mode  !"});
-    res.write(JSON.stringify(data));
-
-    res.end();
+    // res.write();
+    // res.statusCode= 404; 
+    res.end(JSON.stringify(data));
 }
 );
 
