7f863f7a61a347ca73c557f7b6654e13dde085af
mohmdloai <mhmd.00.eng@gmail.com>
2025-06-09 06:59:09 +0300

    Created newserver to make an api

---
+++ newserv.js
@@ -0,0 +1,29 @@
+import { createServer } from 'http';
+const PORT = process.env.PORT;
+
+const users = [
+	{id : 1, name : 'Ahmed one '},
+	{id : 2, name : 'Ahmed Two '},
+	{id : 3, name : 'Ahmed Three '},
+	{id : 4, name : 'Ahmed Yala '}
+]
+
+const server = createServer(
+	(req,res)=>{
+		if(req.url === '/api/users' && req.method === 'GET'){
+			res.writeHead(200, {"Content-Type":"application/json"});
+			res.write(JSON.stringify(users));
+			res.end();
+		}else{
+			res.writeHead(404, {"Content-Type":"application/json"});
+			res.write(JSON.stringify({"mesg":"404 Not Found !"}));
+			res.end();
+		}
+	}
+	);
+
+server.listen(
+	PORT, ()=>{
+		console.log(`listening on port ${PORT}`);
+	}
+	);
