325bdc6ce4e90bcea9c93112b2e777ca5be9b16e
mohmdloai <mhmd.00.eng@gmail.com>
2025-06-11 20:01:14 +0300

    Print process

--- crypto.js
+++ crypto.js
@@ -1,11 +1,24 @@
-import crypto from 'crypto';
+// import crypto from 'crypto';
 
-const hashed = crypto.createHash('sha256');
-hashed.update('sql_p@ss@321-node');
-console.log(hashed.digest('hex'));
+// const hashed = crypto.createHash('sha256');
+// hashed.update('sql_p@ss@321-node');
+// console.log(hashed.digest('hex'));
 
 
-crypto.randomBytes(102400, (err, buffer)=>{
-	if(err) throw err;
-	console.log(buffer.toString('hex'));
-});
+// crypto.randomBytes(102400, (err, buffer)=>{
+// 	if(err) throw err;
+// 	console.log(buffer.toString('hex'));
+// });
+
+
+// process obj
+
+console.log(process);
+console.log(process.argv);
+// [
+//   '/home/arch-linux-mac/.nvm/versions/node/v22.16.0/bin/node',
+//   '/home/arch-linux-mac/projects/nodjs-trs/crypto.js'
+// ]
+
+console.log(process.env);
+console.log(process.env.LOGNAME);//=> arch-linux-mac
